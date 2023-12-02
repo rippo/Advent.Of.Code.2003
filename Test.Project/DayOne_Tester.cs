c6ec3b382b4f1be6590fbb00b459d92b13a7ae58
Richard Wilde <richard@wildesoft.net>
2023-12-02 07:59:43 +0000

    Add project files.

---
+++ Test.Project/DayOne_Tester.cs
@@ -0,0 +1,35 @@
+using Advent.Of.Code._2003;
+
+namespace Test.Project
+{
+    public class DayOne_Tester
+    {
+        [Fact]
+        public void Multiple_Test()
+        {
+            var dict = new Dictionary<string, int>
+            {
+                { "two1nine", 29 },
+                { "eightwothree", 83},
+                { "abcone2threexyz", 13 },
+                { "xtwone3four" , 24},
+                { "4nineeightseven2", 42 },
+                { "zoneight234" , 14},
+                { "7pqrstsixteen", 76 },
+                { "2zd155", 25},
+                { "1", 11},
+                { "1abc2", 12 },
+                { "pqr3stu8vwx", 38 },
+                { "a1b2c3d4e5f" , 15},
+                { "treb7uchet", 77 },
+                { "eighthree", 88},
+                { "eightthree", 83 },
+            };
+            foreach (var item in dict)
+            {
+                var result = item.Key.ReplaceWordsWithDigit().FindDigits();
+                Assert.Equal(item.Value, result);
+            }
+        }
+    }
+}
