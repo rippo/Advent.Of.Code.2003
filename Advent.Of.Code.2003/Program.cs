9aeed5a2558b87899b30740b8c3f3cee47f8186c
Richard Wilde <richard@wildesoft.net>
2023-12-02 08:19:52 +0000

    Refactor

--- Advent.Of.Code.2003/Program.cs
+++ Advent.Of.Code.2003/Program.cs
@@ -7,42 +7,5 @@
             Day1.Go();
         }
 
-        private static class Day1
-        {
-            public static void Go()
-            {
-                var filePath = "..\\..\\..\\input-day1.txt";
-                lines = File.ReadAllLines(filePath);
-                PartA();
-                PartB();
-            }
-
-            private static void PartB()
-            {
-                var total = 0;
-                foreach (var line in lines)
-                {
-                    var firstDigit = line.ReplaceWordsWithDigit().FindFirstDigit();
-                    var lastDigit = line.ReplaceWordsWithDigit().FindLastDigit();
-                    total += int.Parse($"{firstDigit}{lastDigit}");
-                }
-
-                Console.WriteLine(total);
-            }
-
-            private static void PartA()
-            {
-                var total = 0;
-                foreach (var line in lines)
-                {
-                    var firstDigit = line.FindFirstDigit();
-                    var lastDigit = line.FindLastDigit();
-                    total += int.Parse($"{firstDigit}{lastDigit}");
-                }
-                Console.WriteLine(total);
-            }
-
-            private static string[] lines;
-        }
     }
 }
