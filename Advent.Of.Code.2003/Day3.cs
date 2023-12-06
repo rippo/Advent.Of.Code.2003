62765d6ae2cf3465713cfb6ece30cca726907500
Richard Wilde <richard@wildesoft.net>
2023-12-06 10:10:05 +0000

    Complete Day 3

--- Advent.Of.Code.2003/Day3.cs
+++ Advent.Of.Code.2003/Day3.cs
@@ -2,28 +2,117 @@
 
 namespace Advent.Of.Code._2003;
 
-public static class Day3
+public class Day3
 {
-    public static void Go(string file)
+    public void Go()
     {
-        var filePath = $"..\\..\\..\\{file}";
+        var filePath = $"..\\..\\..\\input-day3.txt";
         lines = File.ReadAllLines(filePath).ToList();
 
-        //add a blank row top and bottom
+        //add blanks row top and bottom
         var blank = ".".PadRight(lines[0].Length);
         lines.Insert(0, blank);
         lines.Add(blank);
 
+        ResolveLines();
+        PartA();
+        PartB();
+        
+    }
+
+    public int PartB()
+    {
+        var grandTotal = 0;
+
+        for (var i = 1; i < lines.Count - 1; i++)
+        {
+            var i1 = i;
+            var gearsOnLine = parsedSymbols.Where(w => w.LineNumber == i1 && w.Symbol == '*');
+            foreach (var gear in gearsOnLine)
+            {
+                var matches = new List<int>();
+
+                //Get all numbers on line above and below
+                var numbersToCheck = parsedNumbers.Where(w => w.LineNumber >= gear.LineNumber - 1 && w.LineNumber <= gear.LineNumber + 1);
+
+                foreach (var numberToCheck in numbersToCheck)
+                {
+                    var numberLen = numberToCheck.Number.ToString().Length;
+
+                    //number is in range of gear -len of gear to +1
+                    var lowPos = gear.SymbolPosition - numberLen;
+                    var highPos = gear.SymbolPosition + 1;
+
+                    if (numberToCheck.NumberPosition >= lowPos &&
+                        numberToCheck.NumberPosition <= highPos)
+                    {
+                        matches.Add(numberToCheck.Number);
+                        if (matches.Count == 2)
+                            break;
+                    }
+                }
+
+                if (matches.Count == 2)
+                {
+                    grandTotal += matches[0] * matches[1];
+                }
+            }
+        }
+
+        Console.WriteLine($"Day 3B: {grandTotal}");
+        return grandTotal;
+    }
+
+    public void ResolveLines()
+    {
         for (var i = 0; i < lines.Count - 1; i++)
         {
             ResolveParsedPieces(i);
         }
-
-        PartA();
-        PartB();
     }
 
-    private static void ResolveParsedPieces(int lineNumber)
+    private void PartA()
+    {
+        var grandTotal = 0;
+        for (var i = 1; i < lines.Count - 1; i++)
+        {
+            foreach (var parsedNumber in parsedNumbers.Where(w => w.LineNumber == i))
+            {
+                var lenNumber = parsedNumber.Number.ToString().Length;
+
+                //Line above
+                foreach (var symbol in parsedSymbols.Where(w => w.LineNumber == i - 1))
+                {
+                    var symbolPos = symbol.SymbolPosition;
+                    if (symbolPos >= parsedNumber.NumberPosition - 1 &&
+                        symbolPos <= parsedNumber.NumberPosition + lenNumber)
+                        grandTotal += parsedNumber.Number;
+                }
+
+                //current line
+                foreach (var symbol in parsedSymbols.Where(w => w.LineNumber == i))
+                {
+                    var symbolPos = symbol.SymbolPosition;
+                    if (symbolPos == parsedNumber.NumberPosition - 1 ||
+                        symbolPos == parsedNumber.NumberPosition + lenNumber)
+                        grandTotal += parsedNumber.Number;
+                }
+
+                //line below
+                foreach (var symbol in parsedSymbols.Where(w => w.LineNumber == i + 1))
+                {
+                    var symbolPos = symbol.SymbolPosition;
+                    if (symbolPos >= parsedNumber.NumberPosition - 1 &&
+                        symbolPos <= parsedNumber.NumberPosition + lenNumber)
+                        grandTotal += parsedNumber.Number;
+                }
+            }
+        }
+        //556057
+        Console.WriteLine($"Day 3A: {grandTotal}");
+    }
+
+    private void ResolveParsedPieces(int lineNumber)
     {
         var pattern = @"\d+";
         var matches = Regex.Matches(lines[lineNumber], pattern);
@@ -40,129 +129,9 @@ public static class Day3
         }
     }
 
-    private static void PartA()
-    {
-        var grandTotal = 0;
-        var grandTotal2 = 0;
-        for (var i = 1; i < lines.Count - 1; i++)
-        {
-            foreach (var parsedNumber in parsedNumbers.Where(w=>w.LineNumber == i))
-            {
-                var lenNumber = parsedNumber.Number.ToString().Length;
-
-                //Line above
-                foreach (var symbol in parsedSymbols.Where(w=>w.LineNumber == i - 1))
-                {
-                    var symbolPos = symbol.SymbolPosition;
-                    if (symbolPos >= parsedNumber.NumberPosition - 1 &&
-                        symbolPos <= parsedNumber.NumberPosition + lenNumber)
-                        grandTotal += parsedNumber.Number;
-                }
-
-                //current line
-                foreach (var symbol in parsedSymbols.Where(w => w.LineNumber == i))
-                {
-                    var symbolPos = symbol.SymbolPosition;
-                    if (symbolPos == parsedNumber.NumberPosition - 1  || 
-                        symbolPos == parsedNumber.NumberPosition + lenNumber )
-                        grandTotal += parsedNumber.Number;
-                }
-
-
-                //line below
-                foreach (var symbol in parsedSymbols.Where(w => w.LineNumber == i + 1))
-                {
-                    var symbolPos = symbol.SymbolPosition;
-                    if (symbolPos >= parsedNumber.NumberPosition - 1 &&
-                        symbolPos <= parsedNumber.NumberPosition + lenNumber)
-                        grandTotal += parsedNumber.Number;
-                }
-            }
-
-        }
-        //556057
-        Console.WriteLine(grandTotal);
-    }
-
-    private static void PartB()
-    {
-        var grandTotal = 0;
-        var stars = parsedSymbols.Where(w => w.Symbol == '*').ToList();
-
-        for (var i = 1; i < lines.Count - 1; i++)
-        {
-            foreach (var symbol in stars.Where(w => w.LineNumber == i))
-            {
-                var symbolPos = symbol.SymbolPosition;
-
-                var topNumber = parsedNumbers.FirstOrDefault(w => w.LineNumber == i - 1 &&
-                                                                  w.NumberPosition >= symbolPos - w.Number.ToString().Length &&
-                                                                  w.NumberPosition <= symbolPos + 1);
-
-                var bottomNumber = parsedNumbers.FirstOrDefault(w => w.LineNumber == i + 1 &&
-                                                                  w.NumberPosition >= symbolPos - w.Number.ToString().Length &&
-                                                                  w.NumberPosition <= symbolPos + 1);
-
-                if (topNumber != null && bottomNumber != null)
-                {
-                    grandTotal += topNumber.Number * bottomNumber.Number;
-                }
-
-            }
-        }
-        Console.WriteLine($"WRONG Part 3b: {grandTotal}");
-
-
-        int sum = 0;
-        Match[][] numGrid = new Match[142][];
-        for (int i = 1; i < lines.Count - 1; ++i)
-        {
-            numGrid[i] = new Match[142];
-            MatchCollection numbers = Regex.Matches(lines[i], @"\d+");
-            foreach (Match number in numbers)
-            {
-                for (int j = number.Index; j < number.Index + number.Length; ++j)
-                {
-                    numGrid[i][j] = number;
-                }
-            }
-        }
-
-        for (int i = 1; i < lines.Count - 1; ++i)
-        {
-            MatchCollection gears = Regex.Matches(lines[i], @"\*");
-            foreach (Match gear in gears)
-            {
-                Dictionary<Match, int> partNums = new Dictionary<Match, int>();
-                foreach (Match[] row in numGrid[(i - 1)..(i + 2)])
-                {
-                    if (row == null) continue;
-
-                    foreach (Match m in row[(gear.Index - 1)..(gear.Index + 2)])
-                    {
-                        if (m != null && !partNums.ContainsKey(m))
-                        {
-                            partNums[m] = int.Parse(m.Value);
-                        }
-                    }
-                }
-
-                if (partNums.Count == 2)
-                {
-                    int[] nums = partNums.Values.ToArray();
-                    sum += nums[0] * nums[1];
-                }
-            }
-        }
-        
-        //82824352 RIGHT HOW TO GET THIS
-        Console.WriteLine($"Part 3b: {sum}");
-
-    }
-
-    private static List<string> lines = new();
-    private static readonly List<ParsedNumbers> parsedNumbers = new();
-    private static readonly List<ParsedSymbols> parsedSymbols = new();
+    public List<string> lines = new();
+    private readonly List<ParsedNumbers> parsedNumbers = new();
+    private readonly List<ParsedSymbols> parsedSymbols = new();
 
     private record ParsedNumbers(int LineNumber, int Number, int NumberPosition);
 
--- Test.Project/DayThreeTester.cs
+++ Test.Project/DayThreeTester.cs
@@ -5,9 +5,33 @@ namespace Test.Project;
 public class DayThreeTester
 {
     [Fact]
-    public void Test()
+    public void Test_Sample()
     {
-        Day3.Go("input-day3-test.txt");
+        var d = SetUp("input-day3-test.txt");
+        var a = d.PartB();
+        Assert.Equal(467835, a);
+    }
 
+    [Fact]
+    public void Test_Small()
+    {
+        var d= SetUp("input-day3-small.txt");
+        var a = d.PartB();
+        Assert.Equal(2418179, a);
+    }
+
+
+    private Day3 SetUp(string file)
+    {
+        var filePath = $"..\\..\\..\\{file}";
+        var d = new Day3
+        {
+            lines = File.ReadAllLines(filePath).ToList()
+        };
+        var blank = ".".PadRight(d.lines[0].Length);
+        d.lines.Insert(0, blank);
+        d.lines.Add(blank);
+        d.ResolveLines();
+        return d;
     }
 }
